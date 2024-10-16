ca771f57e89b7d205049ed4013b9d2e98f923372
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2024-10-16 10:01:59 +0800

    🍱add frontend/wailsjs/go

---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,109 @@
+export namespace main {
+	
+	export class PrimerPair {
+	    // Go type: util
+	    primer5F?: any;
+	    // Go type: util
+	    primer3R?: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new PrimerPair(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.primer5F = this.convertValues(source["primer5F"], null);
+	        this.primer3R = this.convertValues(source["primer3R"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class Result {
+	    index: number;
+	    status: string;
+	    name: string;
+	    seq: string;
+	    capturePrimers: PrimerPair[];
+	
+	    static createFrom(source: any = {}) {
+	        return new Result(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.index = source["index"];
+	        this.status = source["status"];
+	        this.name = source["name"];
+	        this.seq = source["seq"];
+	        this.capturePrimers = this.convertValues(source["capturePrimers"], PrimerPair);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class AllResult {
+	    status: string;
+	    results: Result[];
+	
+	    static createFrom(source: any = {}) {
+	        return new AllResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.status = source["status"];
+	        this.results = this.convertValues(source["results"], Result);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
